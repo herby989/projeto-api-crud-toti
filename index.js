7e33a89c51775c18ef352474467f95e1a28dfcaf
Herby Dubic <herby.dubic@gmail.com>
2023-11-28 23:51:38 -0300

    criar um novo arquivo .env para as variaveis de autenticacao

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -15,6 +17,8 @@ app.use(
 
 app.use(express.json());
 
+//Rota da documentação OPEN API Swagger
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //rotas da API
@@ -25,9 +29,12 @@ app.use("/contact", contactRoutes);
 
 // entregar uma porta
 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
 mongoose
   .connect(
-    
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.pyfdgkp.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("Conectamos ao MongoDB!");
